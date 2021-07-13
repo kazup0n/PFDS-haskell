8339140e67329ea5399e90228f36ebefa8163a45
Kazuhiro Sasaki <sasaki.kazuhiro@classmethod.jp>
2021-07-13 12:35:20 +0000

    exercise 2.3

--- src/Tree.hs
+++ src/Tree.hs
@@ -1,21 +1,37 @@
 {-# LANGUAGE GADTs #-}
-
 module Tree where
+import Data.Maybe ( fromMaybe )
 
-data Tree a where
-  E :: Tree a
-  T :: Tree a -> a -> Tree a -> Tree a
+data Tree a = E | T (Tree a) a (Tree a) deriving (Show, Eq)
 
 member :: Ord a => a -> Tree a -> Bool
 member _ E = False
 member x (T a y b)
   | x < y = member x a
-  | x > y = member x a
+  | x > y = member x b
   | otherwise = True
+
+-- quiz 2.2
+member2 :: Ord a => a -> Tree a -> Bool
+member2 _ E = False
+member2 x (T left y right)
+  | x >= y = case right of
+                E -> x == y
+                _ -> member2 x right
+  | otherwise = member2 x left
 
 insert :: Ord a => a -> Tree a -> Tree a
 insert x E = T E x E
-insert x s@(T a y b)
-  | x < y = T (insert x a) y b
-  | x > y = T a y (insert x b)
+insert x s@(T left y right)
+  | x < y = T (insert x left) y right
+  | x > y = T left y (insert x right)
   | otherwise = s
+
+insert2 :: Ord a => a -> Tree a -> Tree a
+insert2 x t = fromMaybe t (insert2' t)
+  where
+  insert2' E = Just $ T E x E
+  insert2' s@(T left y right)
+    | x < y = Just $ T (insert2 x left) y right
+    | x > y = Just $ T left y (insert2 x right)
+    | otherwise = Nothing
---
+++ test/TreeSpec.hs
@@ -0,0 +1,56 @@
+module TreeSpec (spec) where
+
+import Test.Hspec
+import Tree ( insert,member, member2, insert2, Tree(E, T) )
+  
+spec :: Spec 
+spec = do 
+  describe "insert" $ do
+    it "insert to empty tree" $
+      insert 1 E `shouldBe` T E 1 E
+    it "insert to non-empty tree" $
+      insert 2 (T E 1 E) `shouldBe` T E 1 (T E 2 E)
+    it "insert to full branch" $
+      insert 4 (T (T E 1 E) 2 (T E 3 E)) `shouldBe` T (T E 1 E) 2 (T E 3 (T E 4 E))
+
+  describe "insert2" $ do
+    it "insert to empty tree" $
+      insert2 1 E `shouldBe` T E 1 E
+    it "insert to non-empty tree" $
+      insert2 2 (T E 1 E) `shouldBe` T E 1 (T E 2 E)
+    it "insert to full branch" $
+      insert2 4 (T (T E 1 E) 2 (T E 3 E)) `shouldBe` T (T E 1 E) 2 (T E 3 (T E 4 E))
+    it "returns Nothing for inserting duplicated values" $
+      insert2 1 (T E 1 E) `shouldBe` T E 1 E
+    it "returns Nothing for inserting duplicated values in right branch" $
+      insert2 2 (T E 1 (T E 2 E)) `shouldBe` T E 1 (T E 2 E)
+    it "returns Nothing for inserting duplicated values in left branch" $
+      insert2 1 (T (T E 1 E) 2 E) `shouldBe` T (T E 1 E) 2 E
+      
+
+  describe "member" $ do
+    it "return False when empty" $
+      member 1 E `shouldBe` False
+    it "return True when node contains value" $
+      member 1 (T E 1 E) `shouldBe` True
+    it "return True when right branch contains value" $
+      member 2 (T E 1 (T E 2 E)) `shouldBe` True
+    it "return True when left branch contains value" $
+      member 1 (T (T E 1 E) 2 E) `shouldBe` True
+    it "return False when right branch does not contain value" $
+     member 3 (T E 1 (T E 2 E)) `shouldBe` False
+    it "return False when left branch does not contain value" $
+     member (-1) (T (T E 1 E) 2 E) `shouldBe` False
+    describe "member2" $ do
+      it "return False when empty" $
+        member2 1 E `shouldBe` False
+      it "return True when node contains value" $
+        member2 1 (T E 1 E) `shouldBe` True
+      it "return True when right branch contains value" $
+        member2 2 (T E 1 (T E 2 E)) `shouldBe` True
+      it "return True when left branch contains value" $
+        member2 1 (T (T E 1 E) 2 E) `shouldBe` True
+      it "return False when right branch does not contain value" $
+        member2 3 (T E 1 (T E 2 E)) `shouldBe` False
+      it "return False when left branch does not contain value" $
+        member2 (-1) (T (T E 1 E) 2 E) `shouldBe` False     
