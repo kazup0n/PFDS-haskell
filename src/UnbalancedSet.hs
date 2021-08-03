7136f3bc4ad06733fa357e60a2247d440066b0e8
Kazuhiro Sasaki <sasaki.kazuhiro@classmethod.jp>
2021-08-03 08:14:34 +0000

    add Set and UnbalancedSet from appendix

---
+++ src/UnbalancedSet.hs
@@ -0,0 +1,20 @@
+
+{-# OPTIONS_GHC -XMultiParamTypeClasses #-}
+{-# OPTIONS_GHC -XFlexibleInstances #-}
+
+module UnbalancedSet (UnbalancedSet) where
+  import Set
+  data UnbalancedSet a = E | T (UnbalancedSet a) a (UnbalancedSet a)
+  instance Ord a => Set UnbalancedSet a where
+
+    empty = E
+  
+    member x E = False
+    member x (T a y b) | x < y = member x a
+                       | x > y = member x b
+                       | otherwise = True
+    
+    insert x E = T E x E
+    insert x s@(T a y b) | x < y = T (insert x a) y b
+                         | x > y =  T a y (insert x b)
+                         | otherwise = s
