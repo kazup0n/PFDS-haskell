bb85d068c5019665b76593cf2966c1e77e48c122
Kazuhiro Sasaki <sasaki.kazuhiro@classmethod.jp>
2022-07-24 07:55:40 +0000

    fix error of ExplictMinHeap

--- src/ExplicitMin.hs
+++ src/ExplicitMin.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE ExistentialQuantification #-}
-
 module ExplicitMin where
 
 import Heap (Heap, deleteMin, empty, findMin, insert, isEmpty, merge)
@@ -8,28 +6,31 @@ data ExplicitMin h a
   = E
   | T a (h a)
 
-data EM a = forall t. Heap t => EM (ExplicitMin t a)
+instance Heap h => Heap (ExplicitMin h) where
+  empty = E
 
-instance Heap EM where
-  empty = EM E
-  isEmpty (EM E) = True
-  isEmpty (EM _) = False
-  findMin (EM E) = Nothing
-  findMin (EM (T a _)) = Just a
-  insert a (EM E) = EM (T a (insert a empty))
-  insert a (EM (T _ h)) =
+  isEmpty E = True
+  isEmpty _ = False
+
+  findMin E = Nothing
+  findMin (T a _) = Just a
+
+  insert a E = T a (insert a empty)
+  insert a (T _ h) =
     let h' = insert a h
         a' = findMin h'
      in case a' of
-          Just m -> EM (T m h')
-          Nothing -> (EM E)
-  merge (EM (T a h)) (EM E) = EM (T a h)
-  merge (EM E) (EM (T a h)) = EM (T a h)
-  merge (EM (T a1 h1)) (EM (T a2 h2))
-    | a1 > a2 = EM (T a2 (merge h1 h2))
-    | otherwise = EM (T a1 (merge h1 h2))
-  deleteMin (EM E) = Nothing
-  deleteMin (EM (T a h)) = do
+          Just m -> T m h'
+          Nothing -> E
+
+  merge (T a h)  E = T a h
+  merge E (T a h) = T a h
+  merge (T a1 h1) (T a2 h2)
+    | a1 > a2 =  T a2 (merge h1 h2)
+    | otherwise = T a1 (merge h1 h2)
+
+  deleteMin (T a h) = do
     h' <- deleteMin h
     a' <- findMin h'
-    return (EM (T a' h'))
+    return (T a' h')
+  deleteMin _ = Nothing
