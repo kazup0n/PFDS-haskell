b0f2e4c240c373e5bd98966e153d20f4ecf828b9
Kazuhiro Sasaki <sasaki.kazuhiro@classmethod.jp>
2022-08-04 08:52:49 +0000

    演習問題 3.10

--- src/RBTree.hs
+++ src/RBTree.hs
@@ -12,18 +12,31 @@ member x (T _ a y b) |  x < y = member x a
 insert :: (Ord a) => a -> Tree a -> Tree a
 insert x s = T B a y b
   where ins E = (T R E x E)
-        ins s@(T color a y b) | x < y = balance (T color (ins a) y b)
-                              | x > y = balance (T color a y (ins b))
+        ins s@(T color a y b) | x < y, x `lt` value a = llbalance (T color (ins a) y b)
+                              | x < y, x `gt` value a = lrbalance (T color (ins a) y b)
+                              | x > y, x `lt` value b = rlbalance (T color a y (ins b))
+                              | x > y, x `gt` value b = rrbalance (T color a y (ins b))
                               | otherwise = s
         (T _ a y b) = ins s
+        (lt) x m = maybe True (> x) m
+        (gt) x m = maybe True (< x) m
+        value E = Nothing
+        value (T _ _ x _) = Just x
 
+rrbalance :: Tree a -> Tree a
+rrbalance (T B a x (T R b y (T R c z d))) = T R (T B a x b) y (T B c z d)
+rrbalance  x = x
 
-balance :: Tree a -> Tree a
-balance (T B (T R (T R a x b) y c) z d) = T R (T B a x b) y (T B c z d)
-balance (T B (T R a x (T R b y c)) z d) = T R (T B a x b) y (T B c z d)
-balance (T B a x (T R (T R b y c) z d)) = T R (T B a x b) y (T B c z d)
-balance (T B a x (T R b y (T R c z d))) = T R (T B a x b) y (T B c z d)
-balance x = x
+rlbalance :: Tree a -> Tree a
+rlbalance (T B a x (T R b y (T R c z d))) = T R (T B a x b) y (T B c z d)
+rlbalance  x = x
+
+llbalance :: Tree a -> Tree a
+llbalance (T B (T R (T R a x b) y c) z d) = T R (T B a x b) y (T B c z d)
+llbalance x = x
+lrbalance :: Tree a -> Tree a
+lrbalance (T B (T R a x (T R b y c)) z d) = T R (T B a x b) y (T B c z d)
+lrbalance x = x
 
 fromOrdList :: (Ord a) => [a] -> Tree a
 fromOrdList = foldl (\ t a -> insert a t) E
